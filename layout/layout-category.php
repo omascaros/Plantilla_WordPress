88b909db6c78577ff4d8e85ed20fb440fa915cd7
Omascaros <omascaros@gmail.com>
2014-12-02 22:52:49 +0100

    Añadidos los archivos para las categorias

---
+++ layout/layout-category.php
@@ -0,0 +1,25 @@
+<!-- Añadir el html correspondiente a la home -->
+
+<?php 
+	// Importamos el header
+	get_template_part('layout/layout', 'header'); 
+?>
+
+
+<div class="principal">
+	<div class="main">
+		<section class="main__section">
+			<?php 
+				// Importamos el loop
+				get_template_part('loop'); 
+			?>
+		</section>
+	</div> <!-- div main -->
+	
+	<aside class="main__sidebar">
+		<?php 
+			// Importamos el sidebar
+			get_sidebar(); 
+		?>
+	</aside>	
+</div> <!-- div principal -->
